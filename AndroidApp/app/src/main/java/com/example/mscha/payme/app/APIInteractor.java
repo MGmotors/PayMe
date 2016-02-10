ccc4a4258c82d437e3f1e13eafbe65a4c7fb4e4e
Malte Schababerle <Malte Schababerle>
2016-02-10 18:48:10 +0100

    Simple password hash(sha-265) added, api updated, SendAsyncRequest changed

--- AndroidApp/app/src/main/java/com/example/mscha/payme/app/APIInteractor.java
+++ AndroidApp/app/src/main/java/com/example/mscha/payme/app/APIInteractor.java
@@ -1,6 +1,7 @@
 package com.example.mscha.payme.app;
 
 import android.os.AsyncTask;
+import android.util.Base64;
 import android.util.Log;
 
 import com.example.mscha.payme.helper.HTTPPostRequest;
@@ -8,72 +9,95 @@ import com.example.mscha.payme.helper.HTTPPostRequest;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.math.BigInteger;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
 
+import javax.crypto.SecretKeyFactory;
 import javax.net.ssl.HttpsURLConnection;
 
 public class APIInteractor {
 
     public final static String TAG = APIInteractor.class.getName();
-    public final static String MALFORMED_URL_EXCEPTION = "-1";
-    public final static String IOEXCEPTION = "-2";
-    public final static String NULLPOINTER = "-3";
+    public final static String IO_EXCEPTION = "-1";
+    //TODO unschöne Fehlerbehandlung ändern bzw Fehlerbehandlung allgemein nachschauen (listener?)
+    private static String pw;
 
     public void register(String username, String email, String password, OnResponseListener listener) {
         HTTPPostRequest<String, String> request = new HTTPPostRequest<>();
         request.put(API.HeaderFields.ACTION, API.ActionCodes.REGISTER);
         request.put(API.HeaderFields.USERNAME, username);
         request.put(API.HeaderFields.EMAIL, email);
-        request.put(API.HeaderFields.PASSWORD, password);
-        sendAsyncRequest(API.URLs.REGISTER, request, listener);
+        String hashedPassword = byteToString(hash(password));
+        Log.d(TAG, hashedPassword);
+        request.put(API.HeaderFields.PASSWORD, hashedPassword);
+        new SendAsyncRequest(API.URLs.REGISTER, request, listener).execute();
     }
 
     public void login(String username, String password, OnResponseListener listener) {
         HTTPPostRequest<String, String> request = new HTTPPostRequest<>();
         request.put(API.HeaderFields.ACTION, API.ActionCodes.LOGIN);
         request.put(API.HeaderFields.USERNAME, username);
-        request.put(API.HeaderFields.PASSWORD, password);
-        sendAsyncRequest(API.URLs.LOGIN, request, listener);
+        String hashedPassword = byteToString(hash(password));
+        Log.d(TAG, hashedPassword);
+        request.put(API.HeaderFields.PASSWORD, hashedPassword);
+        new SendAsyncRequest(API.URLs.LOGIN, request, listener).execute();
     }
 
-    private void sendAsyncRequest(final String url, final HTTPPostRequest request, final OnResponseListener onResponseListener) {
-
-        new AsyncTask<Void, Void, String>() {
-            @Override
-            protected String doInBackground(Void... params) {
-                try {
-                    HttpsURLConnection connection = (HttpsURLConnection) new URL(url).openConnection();
-                    connection.setDoOutput(true);
-                    connection.setDoInput(true);
-                    connection.getOutputStream().write(request.toString().getBytes());
-                    //TODO debug code entfernen
-                    BufferedReader buff = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-                    String line = "";
-                    String s = "";
-                    while ((line = buff.readLine()) != null)
-                        s += line + "\n";
-                    Log.d(TAG, s);
-                    String response = connection.getHeaderField(API.HeaderFields.ERROR);
-                    if (response != null)
-                        return response;
-                    else
-                        return NULLPOINTER;
-                } catch (MalformedURLException e) {
-                    Log.e(TAG, "MalformedURLException: " + e.getLocalizedMessage());
-                    e.printStackTrace();
-                    return MALFORMED_URL_EXCEPTION;
-                } catch (IOException e) {
-                    Log.e(TAG, "IOException: " + e.getLocalizedMessage());
-                    e.printStackTrace();
-                    return IOEXCEPTION;
-                }
-            }
-
-            @Override
-            protected void onPostExecute(String string) {
-                onResponseListener.onResponse(string);
-            }
-        }.execute();
+    public byte[] hash(String password) {
+        try {
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            return digest.digest(password.getBytes());
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
+
+    public String byteToString(byte[] bytes) {
+        return new String(Base64.decode(bytes, Base64.DEFAULT));
+    }
+
+
+    private class SendAsyncRequest extends AsyncTask<Void, Void, String> {
+        private final String url;
+        private final HTTPPostRequest request;
+        private final OnResponseListener onResponse;
+
+        public SendAsyncRequest(String url, HTTPPostRequest request, OnResponseListener onResponse) {
+            this.url = url;
+            this.request = request;
+            this.onResponse = onResponse;
+        }
+
+        @Override
+        protected String doInBackground(Void... params) {
+            try {
+                HttpsURLConnection connection = (HttpsURLConnection) new URL(url).openConnection();
+                connection.setDoOutput(true);
+                connection.setDoInput(true);
+                connection.getOutputStream().write(request.toString().getBytes());
+                //TODO debug code entfernen
+                BufferedReader buff = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                String line;
+                String s = "";
+                while ((line = buff.readLine()) != null)
+                    s += line + "\n";
+                Log.d(TAG, s);
+                return  connection.getHeaderField(API.HeaderFields.ERROR);
+            } catch (IOException e) {
+                e.printStackTrace();
+                return IO_EXCEPTION;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String string) {
+            onResponse.onResponse(string);
+        }
+    }
+
 }
